9e9e5650eb2bae1d5265b08cfd6f92f951eb6348
Mariusz Matyasik <mariusz.matyasik@outlook.com>
2024-01-09 22:46:23 +0100

    Update database, add migrations

--- ReadingStoreWebApp/Data/ApplicationDbContext.cs
+++ ReadingStoreWebApp/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore;
+using ReadingStoreWebApp.Models;
 
 namespace ReadingStoreWebApp.Data
 {
@@ -9,5 +11,13 @@ namespace ReadingStoreWebApp.Data
             : base(options)
         {
         }
+
+        public DbSet<Genre> Genres{ get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<CartDetail> CartDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<OrderStatus> OrderStatuses { get; set; }
     }
 }
--- ReadingStoreWebApp/Data/Migrations/20240109203019_added-tables.Designer.cs
+++ ReadingStoreWebApp/Data/Migrations/20240109203019_added-tables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReadingStoreWebApp.Data;
 
@@ -11,9 +12,11 @@ using ReadingStoreWebApp.Data;
 namespace ReadingStoreWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240109203019_added-tables")]
+    partial class addedtables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -361,9 +364,6 @@ namespace ReadingStoreWebApp.Data.Migrations
                         .HasColumnType("int");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
-
-                    b.Property<int>("StatusId")
-                        .HasColumnType("int");
 
                     b.Property<string>("StatusName")
                         .IsRequired()
