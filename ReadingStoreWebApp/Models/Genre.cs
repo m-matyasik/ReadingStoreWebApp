d49d9f4d49e6989c859999b93bb4edbec7d52684
Mariusz Matyasik <mariusz.matyasik@outlook.com>
2024-01-09 22:44:44 +0100

    Added Models according to database structure

---
+++ ReadingStoreWebApp/Models/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReadingStoreWebApp.Models
+{
+    [Table("Genre")]
+    public class Genre
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string GenreName { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
