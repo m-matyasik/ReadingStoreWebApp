9968d11cc0132ad13e6c01e5e2cd20feb01f3519
Mariusz Matyasik <mariusz.matyasik@outlook.com>
2024-01-09 23:19:41 +0100

    Add roles and DefaultDataSeed

---
+++ ReadingStoreWebApp/Constants/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace ReadingStoreWebApp.Constants
+{
+    public enum Roles
+    {
+        User,
+        Admin
+    }
+}
---
+++ ReadingStoreWebApp/Data/DbSeeder.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+using ReadingStoreWebApp.Constants;
+
+namespace ReadingStoreWebApp.Data
+{
+    public class DbSeeder
+    {
+        public static async Task SeedDefaultData(IServiceProvider service)
+        {
+            var userMgr = service.GetService<UserManager<IdentityUser>>();
+            var roleMgr = service.GetService<RoleManager<IdentityRole>>();
+
+            // add some roles to db
+
+            await roleMgr.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleMgr.CreateAsync(new IdentityRole(Roles.User.ToString()));
+
+            // create admin user
+            var admin = new IdentityUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                EmailConfirmed = true
+            };
+
+            var userExistsInDb = await userMgr.FindByEmailAsync(admin.Email);
+            if (userExistsInDb is null)
+            {
+                await userMgr.CreateAsync(admin, "Admin7qagcja.");
+                await userMgr.AddToRoleAsync(admin, Roles.Admin.ToString());
+            }
+        }
+    }
+}
