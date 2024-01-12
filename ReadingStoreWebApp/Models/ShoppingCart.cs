69c238580f50bf0c51be6453e1b9a2bdc45fafa1
Mariusz Matyasik <mariusz.matyasik@outlook.com>
2024-01-13 00:38:43 +0100

    Add UnitPrice prop, correct prop name IsDeleted

--- ReadingStoreWebApp/Models/ShoppingCart.cs
+++ ReadingStoreWebApp/Models/ShoppingCart.cs
@@ -9,6 +9,7 @@ namespace ReadingStoreWebApp.Models
         public int Id { get; set; }
         [Required]
         public string UserId{ get; set; }
-        public bool IsDeletedd { get; set; } = false;
+        public bool IsDeleted { get; set; } = false;
+        public ICollection<CartDetail> CartDetails { get; set; }
     }
 }
