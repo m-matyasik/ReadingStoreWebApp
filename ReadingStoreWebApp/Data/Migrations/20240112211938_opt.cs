f86dfe398550b4558e4eb4bfaeb162c741dc7b8f
Mariusz Matyasik <mariusz.matyasik@outlook.com>
2024-01-13 00:43:08 +0100

    Renamed prop migration

---
+++ ReadingStoreWebApp/Data/Migrations/20240112211938_opt.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReadingStoreWebApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class opt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDeletedd",
+                table: "ShoppingCart",
+                newName: "IsDeleted");
+
+            migrationBuilder.AddColumn<double>(
+                name: "UnitPrice",
+                table: "CartDetail",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UnitPrice",
+                table: "CartDetail");
+
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "ShoppingCart",
+                newName: "IsDeletedd");
+        }
+    }
+}
